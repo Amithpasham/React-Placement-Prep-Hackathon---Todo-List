20852010719f073cdcac8da1431293a8f8e44808
Amith <amithpasham@gmail.com>
2021-06-22 21:35:15 +0530

    dele

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,56 @@
-import React from "react";
+// import React from "react";
+// import "./../styles/App.css";
+// import Button1 from './Button1'
+// function App() 
+// {
+// 	const [count, setCount] = useState([])
+// 	function clickHandler(id){
+// 		setCount(count.filter((user,i)=>(i!==parseInt(id))))
+// 	}
+// 	return (
+// 	<div id="main">
+// 			<div>{
+// 			count.map((user,id)=>{
+// 			return < div key={id}>
+// 			<p className="list" >{user}</p>
+// 			<button onClick={()=>clickHandler(id)}>del</button>
+//     		<button onClick={()=>editHandler(id)}>edit</button>
+// 			</div>
+// 			})
+			
+// 		}
+// 		<Button1 fun={setCount}></Button1>
+// 		</div>
+// 	</div>
+// 	);
+// }
+
+
+// export default App;
+import React,{useState} from 'react'
 import "./../styles/App.css";
 import Button1 from './Button1'
-function App() 
-{
+
+function App() {
 	const [count, setCount] = useState([])
 	function clickHandler(id){
 		setCount(count.filter((user,i)=>(i!==parseInt(id))))
 	}
 	return (
-	<div id="main">
-			<div>{
+		<div id="main">
+						<div>{
 			count.map((user,id)=>{
 			return < div key={id}>
 			<p className="list" >{user}</p>
 			<button onClick={()=>clickHandler(id)}>del</button>
-    		<button onClick={()=>editHandler(id)}>edit</button>
 			</div>
 			})
 			
 		}
 		<Button1 fun={setCount}></Button1>
 		</div>
-	</div>
-	);
+			</div>
+	)
 }
 
-
-export default App;
+export default App
